e45bcce6def1936b6d94aaa696c47d6ac93bb93a
Trevor Vandere <tvand08@hotmail.com>
2017-03-22 22:05:06 -0400

    Cool snowday Feature

--- scripts/main.js
+++ scripts/main.js
@@ -36,10 +36,8 @@ jQuery(document).ready(function($) {
 function snackbar() {
     // Get the snackbar DIV
     var x = document.getElementById("snackbar")
-
     // Add the "show" class to DIV
     x.className = "show";
-
     // After 3 seconds, remove the show class from DIV
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
